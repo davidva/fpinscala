ebc515918ef88b058379d554bb52c4d57f2e3e66
David Vilar <davidv@shutl.com>
2014-12-10 22:03:55 +0000

    rewriting Tree functions using fold

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,24 +6,21 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
-  def size[A](t: Tree[A]): Int = t match {
-    case Leaf(_) => 1
-    case Branch(l, r) => 1 + size(l) + size(r)
-  }
+  def size[A](t: Tree[A]): Int =
+    fold(t)(_ => 1)(_ + _ + 1)
 
-  def maximum(t: Tree[Int]): Int = t match {
-    case Leaf(a) => a
-    case Branch(l, r) => maximum(l) max maximum(r)
-  }
+  def maximum(t: Tree[Int]): Int =
+    fold(t)(a => a)(_ max _)
 
-  def depth[A](t: Tree[A]): Int = t match {
-    case Leaf(_) => 1
-    case Branch(l, r) => 1 + depth(l) max depth(r)
-  }
+  def depth[A](t: Tree[A]): Int =
+    fold(t)(_ => 1)((x,y) => (x max y) + 1)
 
-  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
-    case Leaf(a) => Leaf(f(a))
-    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_,_))
+
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match {
+    case Leaf(a) => f(a)
+    case Branch(l,r) => g(fold(l)(f)(g), fold(r)(f)(g))
   }
 }
 
