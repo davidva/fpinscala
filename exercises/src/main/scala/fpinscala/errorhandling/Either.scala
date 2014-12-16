9de434166eb47db638885fd014fc0ce790a2fbac
David Vilar <davidv@shutl.com>
2014-12-16 16:34:11 +0000

    adding sequence and traverse for Either

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -4,27 +4,28 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = this match {
-   case Left(e) => Left(e)
-   case Right(a) => Right(f(a))
- }
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => Right(f(a))
+  }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
-   case Left(e) => Left(e)
-   case Right(a) => f(a)
- }
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => Left(e)
+    case Right(a) => f(a)
+  }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
-  case Left(_) => b
-  case _ => this
- }
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case _ => this
+  }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = (this, b) match {
-  case (Left(e), _) => Left(e)
-  case (_, Left(e)) => Left(e)
-  case (Right(a), Right(b)) => Right(f(a, b))
- }
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = (this, b) match {
+    case (Left(e), _) => Left(e)
+    case (_, Left(e)) => Left(e)
+    case (Right(a), Right(b)) => Right(f(a, b))
+  }
 }
+
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
 
@@ -43,6 +44,14 @@ object Either {
     try Right(a)
     catch { case e: Exception => Left(e) }
 
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    traverse(es)(identity)
+
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    as match {
+      case Nil => Right(Nil)
+      case a :: ta => f(a) flatMap (b => traverse(ta)(f) map (tb => b :: tb))
+    }
 }
 
 object EitherTest {
@@ -74,5 +83,17 @@ object EitherTest {
     test("map2")(left.map2(right2)(append))(left)
     test("map2")(right0.map2(left)(append))(left)
     test("map2")(right0.map2(right2)(append))(Right("02"))
+
+    test("sequence")(Either.sequence(List(Right(1),Left("left"),Right(3))))(Left("left"))
+    test("sequence")(Either.sequence(List(Right(1),Right(2),Right(3))))(Right(List(1,2,3)))
+
+    def safeToInt(i: String): Either[String, Int] =
+      try {
+        Right(i.toInt)
+      } catch {
+        case e: Exception => Left("not a number")
+      }
+    test("traverse")(Either.traverse(List("1","a"))(safeToInt))(Left("not a number"))
+    test("traverse")(Either.traverse(List("1","2"))(safeToInt))(Right(List(1,2)))
   }
 }
