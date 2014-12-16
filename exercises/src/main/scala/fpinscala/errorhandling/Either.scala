34a19f5aee7eb99b2ae0f1256af9b98f46b63e35
David Vilar <summun@gmail.com>
2014-12-16 15:13:39 +0000

    adding tests for either map

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -4,7 +4,10 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = sys.error("todo")
+ def map[B](f: A => B): Either[E, B] = this match {
+   case Left(a) => Left(a)
+   case Right(a) => Right(f(a))
+ }
 
  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = sys.error("todo")
 
@@ -16,13 +19,13 @@ case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
-  def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
-    if (xs.isEmpty) 
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] =
+    if (xs.isEmpty)
       Left("mean of empty list!")
-    else 
+    else
       Right(xs.sum / xs.length)
 
-  def safeDiv(x: Int, y: Int): Either[Exception, Int] = 
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] =
     try Right(x / y)
     catch { case e: Exception => Left(e) }
 
@@ -30,4 +33,18 @@ object Either {
     try Right(a)
     catch { case e: Exception => Left(e) }
 
+}
+
+object EitherTest {
+  def test(name: String)(actual: Any)(expected: Any) {
+    if (actual == expected)
+      println(s"$name works!")
+    else
+      println(s"$name fails - $actual is not $expected")
+  }
+
+  def main(args: Array[String]): Unit = {
+    test("map")(Left("I'm left") map ((x: Int) => x * 2))(Left("I'm left"))
+    test("map")(Right(2) map (_ * 2))(Right(4))
+  }
 }
